b6e300ee067b9d670b01cd6542f663a6f115b158
Faraz Ahmad <farazahmad759@gmail.com>
2020-12-16 00:32:21 +0500

    Some more work done on Form UI

--- examples/test/client/app-2/src/pages/admin/Tasks.edit.js
+++ examples/test/client/app-2/src/pages/admin/Tasks.edit.js
@@ -1,7 +1,31 @@
-import React from "react";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { AdminForm } from "../../components/components.export";
-import { data as formData } from "../../datasource/tasks.form.data";
+// import { data as formData } from "../../datasource/tasks.form.data";
 export const Tasks = () => {
+  const [formData, setFormData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/tasks/1").then((res) => {
+      let _data = res.data.data;
+      let data = [];
+      Object.keys(_data).forEach((_key) => {
+        let _field = {
+          name: _key,
+          title: _key,
+          required: false,
+          placeholder: "",
+          initialValue: _data[_key],
+          type: "string",
+        };
+        data.push(_field);
+      });
+      setFormData(data);
+      console.log(res.data.data);
+      // setFormData(res.data.data);
+    });
+  }, []);
   return (
     <div>
       Edit Task
